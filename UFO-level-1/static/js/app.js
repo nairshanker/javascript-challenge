72a3adcbd94464a775d5adb7d6fdedf27b79bdb7
nairshanker <nair.shanker@gmail.com>
2020-08-30 21:07:04 -0500

    Pushing java script homework

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,23 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tablebody = d3.select("tbody");
+function createtable(data){
+    tablebody.html("")
+    data.forEach(datarow => {
+        var row = tablebody.append("tr")
+        Object.values(datarow).forEach(value => {
+            var cell = row.append("td")
+            cell.text(value)
+        })
+    })
+}
+createtable(tableData)
+
+d3.selectAll("#filter-btn").on("click",handleClick);
+
+function handleClick(){
+    var datevalue = d3.select("#datetime").property("value")
+    filtereddata = tableData.filter(row => row.datetime === datevalue)
+    createtable(filtereddata)
+}
